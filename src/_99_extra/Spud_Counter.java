e2bdbea9c4185e1beb88404b9556038d37af1ddb
League Student <league@iMac-17.attlocal.net>
2019-11-10 15:56:01 -0800

    finished

--- src/_99_extra/Spud_Counter.java
+++ src/_99_extra/Spud_Counter.java
@@ -2,10 +2,15 @@ package _99_extra;
 
 public class Spud_Counter {
 public static void main(String[] args) {
-	for (int i=1; i<8; i++) {
-		System.out.println (i+ " " + "potato,");
-		if (i<=6) {
-			System.out.println(" ");
+	for (int i=1; i<=8; i++) {
+		if (i==4) {
+		System.out.print(i+", ");
+	}
+		else if (i==8) {
+			System.out.print("more");
+		}
+		else {
+			System.out.print(i+" "+"potato, ");
 		}
 	}
 }
